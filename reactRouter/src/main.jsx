03cb6adf9fac226994c2124b397699d8249dc8b1
rp-axelor <r.purabia@axelor.com>
2024-02-06 11:55:09 +0530

    refactor:Apply code prettier

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import User from './components/User'
-import Github, { githubInfoLoader } from './components/Github'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import User from "./components/User";
+import Github, { githubInfoLoader } from "./components/Github";
 // import './App.css'
 // const router = createBrowserRouter([
 //   {
@@ -31,28 +36,24 @@ import Github, { githubInfoLoader } from './components/Github'
 //   }
 // ])
 
-
-const router = createBrowserRouter (
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element= {<Home/>} />
-      <Route path='about' element= {<About/>} />
-      <Route path='contact' element= {<Contact/>} />
-      <Route path='user/:id' element= {<User/>} />
-      <Route 
-      loader={githubInfoLoader} //use Loader to fetch data when your mouse hover on Github in NavBar before clicking this will eventually increase speed
-      path='github' 
-      element= {<Github/>} 
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:id" element={<User />} />
+      <Route
+        loader={githubInfoLoader} //use Loader to fetch data when your mouse hover on Github in NavBar before clicking this will eventually increase speed
+        path="github"
+        element={<Github />}
       />
-      
     </Route>
   )
-  
-)
+);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
